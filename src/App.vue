49d434b3844aa0733cf242030afe7fd564d071c1
Dawoodikram482 <mianmdawood482@gmail.com>
2024-05-27 04:40:18 +0200

    axios-auth added. bootstrap imported. login page started, Login Session store created

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <template>
-
+  <header>
+    <Navigation v-if="$route.path !== '/login' && $route.path !=='/login/signup'"/>
+  </header>
+  <RouterView/>
+  <Footer v-if="$route.path !== '/login' && $route.path !=='/login/signup'"/>
 </template>
 <script>
 import {LoginSessionStore} from '@/stores/LoginSession.js'
+import Navigation from "@/components/Navigation.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  setup() {
+    return {
+      loginSessionStore: LoginSessionStore()
+    };
+  },
+  name: "App",
+  components: {
+    Navigation,
+    Footer
+  },
+  mounted() {
+    this.loginSessionStore.localLogin();
+  }
+}
 
 </script>
 <style scoped>
